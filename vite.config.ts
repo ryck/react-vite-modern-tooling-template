f7c360bc310a0b5fb211a39dc2f3fed8bbed7668
Ricardo Gonzalez <rickgc@gmail.com>
2025-10-24 23:17:15 +0100

    ✨ Use React Compiler

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 /// <reference types="vitest" />
 import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), tailwindcss()]
+  plugins: [
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler', {}]]
+      }
+    }),
+    tsconfigPaths(),
+    tailwindcss()
+  ]
 })
